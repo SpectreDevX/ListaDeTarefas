1948f56d59a4fbcddf780c8383887c7290338a5e
gustavo.opereira4 <gustavo.opereira4@JBQ0684360W10-1>
2025-04-29 14:49:35 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace ListaDeTarefas
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtTarefa = new TextBox();
+            txtAdicionar = new Button();
+            lstTarefas = new ListBox();
+            btnRemover = new Button();
+            btnConcluir = new Button();
+            SuspendLayout();
+            // 
+            // txtTarefa
+            // 
+            txtTarefa.Location = new Point(12, 12);
+            txtTarefa.Name = "txtTarefa";
+            txtTarefa.Size = new Size(237, 23);
+            txtTarefa.TabIndex = 0;
+            // 
+            // txtAdicionar
+            // 
+            txtAdicionar.Location = new Point(12, 146);
+            txtAdicionar.Name = "txtAdicionar";
+            txtAdicionar.Size = new Size(75, 23);
+            txtAdicionar.TabIndex = 1;
+            txtAdicionar.Text = "Adicionar";
+            txtAdicionar.UseVisualStyleBackColor = true;
+            txtAdicionar.Click += txtAdicionar_Click;
+            // 
+            // lstTarefas
+            // 
+            lstTarefas.FormattingEnabled = true;
+            lstTarefas.Location = new Point(12, 41);
+            lstTarefas.Name = "lstTarefas";
+            lstTarefas.Size = new Size(237, 94);
+            lstTarefas.TabIndex = 2;
+            // 
+            // btnRemover
+            // 
+            btnRemover.Location = new Point(93, 146);
+            btnRemover.Name = "btnRemover";
+            btnRemover.Size = new Size(75, 23);
+            btnRemover.TabIndex = 3;
+            btnRemover.Text = "Remover";
+            btnRemover.UseVisualStyleBackColor = true;
+            btnRemover.Click += btnRemover_Click;
+            // 
+            // btnConcluir
+            // 
+            btnConcluir.Location = new Point(174, 146);
+            btnConcluir.Name = "btnConcluir";
+            btnConcluir.Size = new Size(75, 23);
+            btnConcluir.TabIndex = 4;
+            btnConcluir.Text = "Concluir";
+            btnConcluir.UseVisualStyleBackColor = true;
+            btnConcluir.Click += btnConcluir_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(284, 191);
+            Controls.Add(btnConcluir);
+            Controls.Add(btnRemover);
+            Controls.Add(lstTarefas);
+            Controls.Add(txtAdicionar);
+            Controls.Add(txtTarefa);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Lista de Tarefas";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtTarefa;
+        private Button txtAdicionar;
+        private ListBox lstTarefas;
+        private Button btnRemover;
+        private Button btnConcluir;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,50 @@
+﻿namespace ListaDeTarefas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtAdicionar_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtTarefa.Text))
+            {
+                lstTarefas.Items.Add(txtTarefa.Text);
+                txtTarefa.Clear();
+                txtTarefa.Focus();
+            }
+            else
+            {
+                MessageBox.Show("Digite uma tarefa.");
+            }
+        }
+
+        private void btnRemover_Click(object sender, EventArgs e)
+        {
+            if (lstTarefas.SelectedIndex != -1)
+            {
+                lstTarefas.Items.RemoveAt(lstTarefas.SelectedIndex);
+            }
+            else
+            {
+                MessageBox.Show("Selecione uma tarefa para remover.");
+            }
+        }
+
+        private void btnConcluir_Click(object sender, EventArgs e)
+        {
+            int index = lstTarefas.SelectedIndex;
+            if (index != -1)
+            {
+                string tarefa = lstTarefas.Items[index].ToString();
+                lstTarefas.Items[index] = "✔️ " + tarefa;
+            }
+            else
+            {
+                MessageBox.Show("Selecione uma tarefa para concluir.");
+            }
+        }
+    }
+}
